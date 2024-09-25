f7ab4195a647a12a01d70fe941153f850a19ecc3
vaishali <vaishalikshirsagar4694@gmail.com>
2024-09-25 10:35:08 +0000

    this file from singapore server in which written the process of send file from local repo to central repo

---
+++ sdemogit.py
@@ -0,0 +1,23 @@
+create and launch instance
+login as server with username ec2-user instance
+use sudo su command to become root user
+create one directory
+use git init to intialize directory as git local repository
+use git config --global user.name "__" to create user for git repo
+use git config --global user.email "__" to link mail to repo"
+use git config --list to check details
+create one file with code -created in git working directory
+now check git status -there is untracked file show
+use git add . - to add file from wokspace to staging area now file is known to git
+use git commit -m "message" - to commit file from stating area to local repository
+use git remote add origin <url> -to connect local repo with central repo github
+use git push -u origin master -to send file from local repo to central repo
+if gives error then first generate ssh key using command-
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+then add ssh key to ssh agent by starting ssh agent - eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+copy ssh key using - cat ~/.ssh/id_rsa.pub
+paste this ssh key into github setting ssh and gpg key "new ssh key" and save
+now taste the ssh connection to github account - ssh -T git@github.com
+now update git remote location - git remote set-url origin git@github.com:Mohandodmise/MohanPracticeDevops.git
+not use git push origin master
